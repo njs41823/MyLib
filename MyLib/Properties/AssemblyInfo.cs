c9a2bdb6d8783c9563e00cdba4249b283bb17820
Nathan Sickle <nathansickle@gmail.com>
2017-12-15 17:43:20 -0500

    Commit immediately following v1.0.0.0 build. Version number in the Asssembly info has been incremented to v1.0.0.1 to prevent any future builds as v1.0.0.0.

--- MyLib/Properties/AssemblyInfo.cs
+++ MyLib/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MyLib")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("The personal development library of Nathan J Sickle")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MyLib")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © 2017 - Nathan J Sickle")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
