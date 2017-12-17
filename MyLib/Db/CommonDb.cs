458b0690de94842678b493063a57e651c5313e17
Nathan Sickle <nathansickle@gmail.com>
2017-12-17 10:23:21 -0500

    The REAL and FINAL v1.0.0.0 commit

---
+++ MyLib/Db/CommonDb.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyLib.Db
+{
+    public class CommonDb
+    {
+        public static T DBNullHandler<T>(object value)
+        {
+            if (DBNull.Value.Equals(value) || !(value is T valueAsT))
+            {
+                return default(T);
+            }
+            return valueAsT;
+        }
+    }
+}
